27e2e0791cae42e80a8b75d96056a8a2ab32981e
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2025-06-18 17:25:37 -0300

    Add files via upload

---
+++ singleproductatp1/data/products.js
@@ -0,0 +1,13 @@
+// src/data/products.js
+export const products = [
+    {
+      id: "1",
+      name: "Camisetas Masculinas",
+      category: "Camisetas",
+      price: "R$139.00",
+      description: "A camiseta Gildan Ultra Cotton é feita de um tecido robusto...",
+      image: "https://raw.githubusercontent.com/tech2etc/Build-and-Deploy-Ecommerce-Website/main/img/products/f1.jpg"
+    },
+    // ...adicione os outros 100 produtos aqui
+  ];
+  
